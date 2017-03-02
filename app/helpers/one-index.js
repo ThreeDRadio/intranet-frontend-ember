81e4c6235bce44e3f8e0572d7b8f3fbbe720e2ad
Michael Marner <michael@20papercups.net>
2017-03-02 19:56:12 +1030

    Use typeof to make test runner work on Travis

--- app/helpers/one-index.js
+++ app/helpers/one-index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function oneIndex(params/*, hash*/) {
   if (params.length > 0) {
-    if (Number.isInteger(params[0])) {
+    if (typeof(params[0]) === 'number') {
       return params[0]+1;
     }
     return params[0];
