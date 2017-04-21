c6e735cbb3ea9ed023dd44ecba8b3d0cf3cfede2
Michael Marner <michael@20papercups.net>
2017-04-21 22:15:19 +0930

    Run latest blueprint

--- config/environment.js
+++ config/environment.js
@@ -1,15 +1,19 @@
-/* jshint node: true */
+/* eslint-env node */
 
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'threedradio-intranet-frontend',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
@@ -40,9 +44,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
-    ENV.backendBaseURL = 'http://localhost:8000/logger/';
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
